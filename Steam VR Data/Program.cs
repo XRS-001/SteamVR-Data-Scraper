7913d4a95d260595bade2f1490ba99de44f4f855
VCR01 <126965254+VCR01@users.noreply.github.com>
2023-06-17 13:45:06 +0100

    Append Data To Google Sheet
    I made it so that it appends the data of active players in supported and VR only games and prints them to two separate sheets, and does the same for peak active players

--- Steam VR Data/Program.cs
+++ Steam VR Data/Program.cs
@@ -3,44 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using HtmlAgilityPack;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using System.IO;
+using Google.Apis.Sheets.v4.Data;
 
-namespace Name{
-    
-    internal class NewBaseType
+internal class NewBaseType
     {
-        static void Main(string[] args)
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string ApplicationName = "Google Sheets Example";
+        static readonly string SpreadsheetId = "1iNCpvySskw4ANIOHQirU8vm66C86sLTGugGrXH1i9dk";
+        static readonly string SheetNameVRSupported = "VRSupported";
+        static readonly string SheetNameVROnly = "VROnly";
+        static readonly string SheetNameVROnlyPeak = "VRSupportedPeak";
+        static readonly string SheetNameVRSupportedPeak = "VROnlyPeak";
+        static readonly string CredentialsPath = "C:\\Users\\Con_P\\Desktop\\Steam VR Data\\webscraper-389916-1e30a707e640.json";
+        private static SheetsService InitializeSheetsService(string credentialsPath)
         {
-            var url1 = "https://store.steampowered.com/search/?tags=21978&supportedlang=english&ndl=1&count=100";
-            var url2 = "https://store.steampowered.com/search/?vrsupport=401&supportedlang=english&ndl=1&count=100";
-            Console.WriteLine("Which option would you like to scrape?");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Option1: active player data from as recent as possible.");
-            Console.WriteLine("Option2: peak active player data from the past 24 hours.");
-            Console.ResetColor();
-            string optionPicked = Console.ReadLine();
-            if(optionPicked.Contains("1")||optionPicked.Contains("first"))
+            GoogleCredential credential;
+
+            using (var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
             {
-                RunProgram(url1, "active players from as recently as possible from the top 100 VR supported games", "/html/body/div[3]/div[3]/div[1]/span");
-                RunProgram(url2, "active players from as recently as possible from the top 100 VR only games", "/html/body/div[3]/div[3]/div[1]/span");
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
             }
-            if(optionPicked.Contains("2")||optionPicked.Contains("second"))
+
+            return new SheetsService(new BaseClientService.Initializer()
             {
-                RunProgram(url1, "peak active players in 24 hours from the top 100 VR supported games", "/html/body/div[3]/div[3]/div[2]/span");
-                RunProgram(url2, "peak active players in 24 hours from the top 100 VR only games", "/html/body/div[3]/div[3]/div[2]/span");
-            } 
-            Console.ReadKey();
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName
+            });
         }
 
-        static void RunProgram(string url, string top100Message,string XPath)
+        private static void AppendData(SheetsService service, string spreadsheetId, string sheetName, IList<object> rowData, int collumA)
         {
+            // Define the range where the data will be appended
+            string range = $"{sheetName}!A{collumA}:B3";
+
+            // Create the value range object
+            var valueRange = new ValueRange
+            {
+                Values = new List<IList<object>> { rowData }
+            };
+
+            // Create the AppendValuesRequest
+            var request = service.Spreadsheets.Values.Append(valueRange, spreadsheetId, range);
+            request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+
+            // Execute the request
+            var response = request.Execute();
+        }
+        static void Main(string[] args)
+        {   
+            var url1 = "https://store.steampowered.com/search/?tags=21978&supportedlang=english&ndl=1&count=100";
+            var url2 = "https://store.steampowered.com/search/?vrsupport=401&supportedlang=english&ndl=1&count=100";
+            RunProgram(url1, "active players from as recently as possible from the top 100 VR supported games", "/html/body/div[3]/div[3]/div[1]/span", SheetNameVRSupported);
+            RunProgram(url2, "active players from as recently as possible from the top 100 VR only games", "/html/body/div[3]/div[3]/div[1]/span", SheetNameVROnly);
+            RunProgram(url1, "peak active players in 24 hours from the top 100 VR supported games", "/html/body/div[3]/div[3]/div[2]/span", SheetNameVRSupportedPeak);
+            RunProgram(url2, "peak active players in 24 hours from the top 100 VR only games", "/html/body/div[3]/div[3]/div[2]/span", SheetNameVROnlyPeak);
+            Console.ReadKey();
+        }
+        static void CallGoogle(string SheetName, List<object> rowData, int collumA)
+        {
+            try
+            {
+                // Initialize the Sheets service
+                SheetsService service = InitializeSheetsService(CredentialsPath);
+
+                // Append the data to the spreadsheet
+                AppendData(service, SpreadsheetId, SheetName, rowData, collumA);
+                Console.WriteLine("Data appended successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+        static void RunProgram(string url, string top100Message, string XPath, string sheetName)
+        {
+            int collumA = 1;
             Console.WriteLine($"Scraping {top100Message} from store.steampowered.com....\n");
-            var games = new List<Tuple<string, float>>();
+            List<object> games = new List<object>();
             float totalMonthlyPlayers = 0;
 
             using (var client = new WebClient())
             {
                 var html = client.DownloadString(url);
-
+                var time = "";
                 var doc = new HtmlDocument();
                 doc.LoadHtml(html);
 
@@ -50,6 +100,7 @@ namespace Name{
                 {
                     var gameName = gameNode.SelectSingleNode(".//span[@class='title']").InnerText;
                     var appId = GetAppId(gameName);
+                    string imageUrl = $"//steamcharts.com/assets/steam-images/{appId}.jpg";
                     var chartUrl = $"https://steamcharts.com/app/{appId}";
                     var chartHtml = "";
 
@@ -58,6 +109,7 @@ namespace Name{
                         try
                         {
                             chartHtml = chartClient.DownloadString(chartUrl);
+                            time = GetTime(chartHtml);
                         }
                         catch (WebException ex)
                         {
@@ -72,7 +124,6 @@ namespace Name{
                             }
                         }
                     }
-
                     var chartDoc = new HtmlDocument();
                     chartDoc.LoadHtml(chartHtml);
 
@@ -80,16 +131,22 @@ namespace Name{
 
                     if (monthlyPlayersNode != null && float.TryParse(monthlyPlayersNode.InnerText, out var monthlyPlayers))
                     {
-                        games.Add(new Tuple<string, float>(gameName, monthlyPlayers));
+                        List<object> game = new List<object>
+                        {
+                            gameName,
+                            monthlyPlayers,
+                            time,
+                            appId,
+                            imageUrl,
+                        };
+                        games.Add(game);
                         totalMonthlyPlayers += monthlyPlayers;
                     }
                     else
                     {
                         Console.WriteLine($"Active players data not found for {gameName}.");
-                    }
+                    } 
                 }
-
-                games = games.OrderByDescending(x => x.Item2 == float.MinValue).ThenByDescending(x => x.Item2).ToList();
 
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"\nTotal Active players: {totalMonthlyPlayers.ToString("F1")}");
@@ -97,11 +154,13 @@ namespace Name{
 
                 Console.WriteLine("Top 100 VR Games on Steam:");
                 int count = 0;
-                foreach (var game in games.Where(g => g.Item2 >= 0).OrderByDescending(g => g.Item2))
+                foreach (List<object> game in games)
                 {
-                    Console.WriteLine($"{game.Item1}: {game.Item2:F1} active players.");
+                    collumA ++;
+                    CallGoogle(sheetName, game, collumA);
+                    Console.WriteLine($"App id: {game[3]} ImageURL: {game[4]} {game[0]}: {game[1]:F1} active players at {game[2]}");
                     count++;
-                    if (count == 200) break;
+                    if (count == 50) break;
                 }
             }
             Console.WriteLine();
@@ -126,9 +185,27 @@ namespace Name{
                 }
                 else
                 {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
                     throw new ArgumentException($"App ID for {gameName} not found.");
+                    Console.ResetColor();
                 }
             }
         }
+        static string GetTime(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var timeNode = doc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div[1]/abbr");
+            if (timeNode != null)
+            {
+                var timeValue = timeNode.Attributes["title"].Value;
+                return timeValue;
+            }
+            else
+            {
+                return "failed";
+            }
+        }
     }
-}
